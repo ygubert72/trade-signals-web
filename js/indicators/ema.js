2c6f5152b55594bd9c399c307e6f047d377c35c8
ygubert72 <ygubert72@gmail.com>
2026-08-05 11:50:04 +0500

    Update ema.js

--- js/indicators/ema.js
+++ js/indicators/ema.js
@@ -1,1 +1,24 @@
+// js/indicators/ema.js
 
+export function calculateEMA(data, period) {
+    if (data.length < period) return [];
+
+    const ema = [];
+    const multiplier = 2 / (period + 1);
+
+    // Первое значение = SMA
+    let sum = 0;
+    for (let i = 0; i < period; i++) sum += data[i];
+    ema[period - 1] = sum / period;
+
+    for (let i = period; i < data.length; i++) {
+        ema[i] = (data[i] - ema[i - 1]) * multiplier + ema[i - 1];
+    }
+
+    return ema;
+}
+
+export function getLastEMA(data, period) {
+    const ema = calculateEMA(data, period);
+    return ema.length ? ema[ema.length - 1] : null;
+}
