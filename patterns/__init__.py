4feb09a813ca3969be24e645b234b4a4d7327e2d
ygubert72 <ygubert72@gmail.com>
2026-08-05 11:30:28 +0500

    Update __init__.py

--- patterns/__init__.py
+++ patterns/__init__.py
@@ -1,1 +1,27 @@
+from .base_pattern import BasePattern
+from .pinbar import PinBarPattern
+from .golden_cross import GoldenCrossPattern
+from .ma_200 import MA200TouchPattern
+from .breakout import BreakoutPattern
+from .double_top_bottom import DoubleTopPattern, DoubleBottomPattern
+from .head_shoulders import HeadShouldersPattern
+from .cup_handle import CupHandlePattern
+from .fractal_breakout import FractalBreakoutPattern, reset_fractal_cache
+from .fake_breakout import FakeBreakoutPattern
+from .registry import PatternRegistry
 
+__all__ = [
+    'BasePattern',
+    'PinBarPattern',
+    'GoldenCrossPattern',
+    'MA200TouchPattern',
+    'BreakoutPattern',
+    'DoubleTopPattern',
+    'DoubleBottomPattern',
+    'HeadShouldersPattern',
+    'CupHandlePattern',
+    'FractalBreakoutPattern',
+    'FakeBreakoutPattern',
+    'reset_fractal_cache',
+    'PatternRegistry'
+]
