4c5d2e03d71fc64e1d2a040ced511092dc6e779d
ygubert72 <ygubert72@gmail.com>
2026-08-05 11:11:51 +0500

    Update factory.py

--- scanners/factory.py
+++ scanners/factory.py
@@ -1,1 +1,19 @@
+# trade-signals-web/scanners/factory.py
 
+from .stock_scanner import StockScanner
+from .crypto_scanner import CryptoScanner
+
+class ScannerFactory:
+    """Фабрика для создания сканеров в зависимости от типа рынка"""
+    
+    @staticmethod
+    def create_scanner(symbol: str, timeframe: str, market_type: str):
+        """
+        Создание сканера
+        market_type: 'crypto', 'russian', 'us', 'futures'
+        """
+        if market_type == 'crypto':
+            return CryptoScanner(symbol, timeframe)
+        else:
+            is_russian = (market_type == 'russian')
+            return StockScanner(symbol, timeframe, is_russian=is_russian)
