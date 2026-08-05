a70eeaff8feb826ff41c1dbbf202585434c97563
ygubert72 <ygubert72@gmail.com>
2026-08-05 11:37:20 +0500

    Delete scanners directory

--- scanners/__init__.py
+++
@@ -1,8 +0,0 @@
-# trade-signals-web/scanners/__init__.py
-
-from .base_scanner import BaseScanner
-from .crypto_scanner import CryptoScanner
-from .stock_scanner import StockScanner
-from .factory import ScannerFactory
-
-__all__ = ['BaseScanner', 'CryptoScanner', 'StockScanner', 'ScannerFactory']
